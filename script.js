1d0718b3e248280aa08dd6401d5deba07c56e2f2
MostsfaMGomaa <gomaamostafa26@gmail.com>
2022-04-28 15:10:49 +0200

    Alpha Home Page

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const btnLogin = document.querySelector(".btnLogin");
 
 btnLogin.addEventListener("click", () => {
-  location.href = "/login/login.html";
+  location.href = "login.html";
+
+  // onclick="location.href='page2.html'"
 });
 
 // btn_login.onclick = document.location = "/login/login.html";
-// btn_login.onclick = function () {
-//   console.log("Hello");
-//   location.href = "/login/login.html";
-// };
+btn_login.onclick = function () {
+  console.log("Hello");
+  location.href = "/login/login.html";
+};
