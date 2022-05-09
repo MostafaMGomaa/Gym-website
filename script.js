345ad93517f52636449bda484e28697dd3a64714
MostsfaMGomaa <gomaamostafa26@gmail.com>
2022-05-09 10:48:49 +0200

    Add gallery page

--- script.js
+++ script.js
@@ -1,14 +1,63 @@
 const btnLogin = document.querySelector(".btnLogin");
 const btnSignup = document.querySelector(".btnSignup");
+const btnPrograms = document.querySelector(".programsBtn");
+const divPrograms = document.querySelector(".programs");
+const divTable = document.querySelector(".table");
+const dropPrograms = document.querySelector(".dropPrograms");
+const dropProgOne = document.querySelector(".dropProgOne");
+const dropProgTwo = document.querySelector(".dropProgTwo");
+const dropProgThree = document.querySelector(".dropProgThree");
+const dropSessions = document.querySelector(".dropSessions");
+const progOne = document.querySelector(".progOne");
+const progTwo = document.querySelector(".progTwo");
+const progThree = document.querySelector(".progThree");
+const h1 = document.querySelector(".display-2");
+const programs = document.querySelector(".programs");
+
+// Check if wanted div hide or not then hide the other divs
+const hide = (fdiv, sdiv) => {
+  if (fdiv.classList.contains("hidden")) fdiv.classList.remove("hidden");
+  sdiv.classList.add("hidden");
+};
+
+// Mark selected div
+const mark = (fDiv, sDiv, tDiv) => {
+  if (sDiv.classList.contains("marked") || tDiv.classList.contains("marked")) {
+    sDiv.classList.remove("marked");
+    tDiv.classList.remove("marked");
+  }
+  fDiv.classList.add("marked");
+};
 
 btnLogin.addEventListener("click", () => {
   location.href = "login/login.html";
 });
+
 btnSignup.addEventListener("click", () => {
   location.href = "registration/registration.html";
 });
 
-// btn_login.onclick = function () {
-//   console.log("Hello");
-//   location.href = "/login/login.html";
-// };
+dropProgOne.addEventListener("click", () => {
+  mark(progOne, progTwo, progThree);
+});
+
+dropProgTwo.addEventListener("click", () => {
+  mark(progTwo, progThree, progOne);
+});
+
+dropProgThree.addEventListener("click", () => {
+  mark(progThree, progTwo, progOne);
+});
+
+dropPrograms.addEventListener("click", () => {
+  h1.innerHTML = "Memberships";
+  hide(divPrograms, divTable);
+  progOne.classList.remove("marked");
+  progTwo.classList.remove("marked");
+  progThree.classList.remove("marked");
+});
+
+dropSessions.addEventListener("click", () => {
+  h1.innerHTML = "Sessions Table";
+  hide(divTable, divPrograms);
+});
